4e45976b74be532dc9297a71efb8f4f4983af3b7
passchaos <greedwolf.dss@gmail.com>
2016-08-28 16:40:42 +0800

    更改代码结构

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,40 @@ use hyper::Client;
 use hyper::header::Connection;
 
 use std::env;
+use std::str;
 
-use rocksdb::Writable;
-
-// use log::LogLevel;
+use rocksdb::{DB, Writable};
 
 const REQUEST_BASE: &'static str = "http://fanyi.youdao.com/openapi.do?keyfrom=ydcv-rust&key=379421805&type=data&doctype=json&version=1.1&q=";
+
+fn get_remote_json_translation(query: &str, cache_db: &DB) -> Result<String, String> {
+    let mut request_url = REQUEST_BASE.to_string();
+
+    request_url.push_str(query);
+
+    let client = Client::new();
+
+    info!("开始从网络获取翻译结果");
+    let mut res = match client.get(&request_url).header(Connection::close()).send() {
+        Ok(noun) => noun,
+        Err(err) => {
+            println!("网络错误，请稍候重试！ {}", err);
+            return Err(format!("{}", err))
+        },
+    };
+
+    let mut body = String::new();
+    res.read_to_string(&mut body).expect("网络响应中没有字符段");
+
+    debug!("json content: {}", body);
+
+    let trans_result: trans_type::Translation = serde_json::from_str(body.as_str()).expect("需要更新本地json格式处理");
+
+    info!("将 「{}」 的翻译结果写入缓存", query);
+    cache_db.put(query.as_bytes(), format!("{}", trans_result).as_bytes());
+    
+    Ok(format!("{}", trans_result))
+}
 
 fn main() {
     env_logger::init().unwrap();
@@ -40,40 +68,18 @@ fn main() {
         }
     };
 
-    let db = rocksdb::DB::open_default("/home/passchaos/.cache/ydcv/cache").unwrap();
+    let db = DB::open_default("/home/passchaos/.cache/ydcv/cache").unwrap();
 
     let db_key = arg.as_bytes();
-    if let Ok(Some(value)) = db.get(db_key) {
-        info!("从本地缓存读取");
-        println!("{}", value.to_utf8().unwrap());
-        return
-    }
 
-    info!("开始从网络获取翻译结果");
-    
-    let mut request_url = REQUEST_BASE.to_string();
-
-    request_url.push_str(arg.as_str());
-
-    let client = Client::new();
-
-    let mut res = match client.get(&request_url).header(Connection::close()).send() {
-        Ok(noun) => noun,
-        Err(err) => {
-            println!("网络错误，请稍候重试！ {}", err);
-            return
+    match db.get(db_key) {
+        Ok(Some(value)) => {
+            info!("从本地缓存读取");
+            println!("{}", value.to_utf8().unwrap());            
+        },
+        _ => {
+            let translation = get_remote_json_translation(arg.as_str(), &db);
+            println!("{}", translation.unwrap_or("网络出错".to_string()));
         }
-    };
-
-    let mut body = String::new();
-    res.read_to_string(&mut body).expect("网络响应中没有字符段");
-
-    debug!("json content: {}", body);
-
-    let trans_result: trans_type::Translation = serde_json::from_str(body.as_str()).expect("需要更新本地json格式处理");
-    match db.put(db_key, format!("{}", trans_result).as_bytes()) {
-        Ok(value) => info!("{:?} 写入缓存", value),
-        Err(e) => error!("{}", e),
-    };
-    println!("{}", trans_result);
+    }
 }

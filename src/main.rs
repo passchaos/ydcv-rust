0ce072b0a2873a31fca512ba961bc7215def28a3
passchaos <greedwolf.dss@gmail.com>
2016-09-11 12:18:13 +0800

    使用try!处理错误

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,13 @@ use std::str;
 
 use rocksdb::{DB, Writable, WriteBatch};
 
+#[derive(Debug)]
+enum YDCVError {
+    Network,
+    Json,
+    Cache,
+}
+
 const REQUEST_BASE: &'static str = "http://fanyi.youdao.com/openapi.do?keyfrom=ydcv-rust&key=379421805&type=data&doctype=json&version=1.1&q=";
 
 fn get_remote_json_translation(query: &str, cache_db: &DB, update_cache: bool) -> Result<String, String> {
@@ -38,20 +45,15 @@ fn get_remote_json_translation(query: &str, cache_db: &DB, update_cache: bool) -
     let client = Client::new();
 
     info!("开始从网络获取翻译结果");
-    let mut res = match client.get(&request_url).header(Connection::close()).send() {
-        Ok(noun) => noun,
-        Err(err) => {
-            println!("网络错误，请稍候重试！ {}", err);
-            return Err(format!("{}", err))
-        },
-    };
+
+    let mut res = try!(client.get(&request_url).header(Connection::close()).send().map_err(|e| e.to_string()));
 
     let mut body = String::new();
-    res.read_to_string(&mut body).expect("网络响应中没有字符段");
+    try!(res.read_to_string(&mut body).map_err(|e| e.to_string()));
 
     debug!("json content: {}", body);
 
-    let trans_result: trans_type::Translation = serde_json::from_str(body.as_str()).expect("需要更新本地json格式处理");
+    let trans_result: trans_type::Translation = try!(serde_json::from_str(body.as_str()).map_err(|e| e.to_string()));
 
     if update_cache {
         debug!("更新本地缓存");
@@ -61,10 +63,7 @@ fn get_remote_json_translation(query: &str, cache_db: &DB, update_cache: bool) -
         cache_db.write(batch);
     } else {
         debug!("写入本地缓存");
-        match cache_db.put(query.as_bytes(), format!("{}", trans_result).as_bytes()) {
-            Err(err) => info!("{}", err),
-            _ => {},        
-        };        
+        try!(cache_db.put(query.as_bytes(), format!("{}", trans_result).as_bytes()));
     }
     
     Ok(format!("{}", trans_result))
